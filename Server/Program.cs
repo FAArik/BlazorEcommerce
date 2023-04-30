40a9cc12efd2c677e483a7edee57fcfb5796ea01
FAArik <62200015+FAArik@users.noreply.github.com>
2023-04-30 19:37:19 +0300

    79

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,9 +2,9 @@ global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerce.Server.Data;
 global using BlazorEcommerce.Server.Services.ProductService;
+global using BlazorEcommerce.Server.Services.CartService;
 global using BlazorEcommerce.Server.Services.CategoryService;
 using Microsoft.AspNetCore.ResponseCompression;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +21,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
+
 var app = builder.Build();
 
 app.UseSwaggerUI();
