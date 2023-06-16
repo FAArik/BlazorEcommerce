c6d1e20699d8ad844947f48c78472e1eed4c709c
FAArik <62200015+FAArik@users.noreply.github.com>
2023-06-16 12:47:06 +0300

    120

--- Client/Services/AuthService/IAuthService.cs
+++ Client/Services/AuthService/IAuthService.cs
@@ -4,5 +4,6 @@
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
         Task<ServiceResponse<string>> Login(UserLogin login);
+        Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request);
     }
 }
